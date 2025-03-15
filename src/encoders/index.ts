07f46432c619e76ccb32aa182851589fa0e89370
Joel Wetzell <jwetzell@yahoo.com>
2025-03-15 08:32:19 -0500

    add encoder for trackable sub module

--- src/encoders/index.ts
+++ src/encoders/index.ts
@@ -1,7 +1,9 @@
 import RTTrPM from './motion/rttrpm';
+import Trackable from './motion/trackable';
 import RTTrPHeader from './rttrp-header';
 
 export const Encoders = {
   RTTrPHeader,
   RTTrPM,
+  Trackable,
 };
