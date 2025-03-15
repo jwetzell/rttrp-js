b99a0224e8fe2a15783362426a1d2ca55261987f
Joel Wetzell <jwetzell@yahoo.com>
2025-03-15 08:33:41 -0500

    add encoder for orientation sub modules

--- src/encoders/index.ts
+++ src/encoders/index.ts
@@ -1,4 +1,6 @@
 import CentroidPosition from './motion/centroid-position';
+import OrientationEuler from './motion/orientation-euler';
+import OrientationQuaternion from './motion/orientation-quaternion';
 import RTTrPM from './motion/rttrpm';
 import Trackable from './motion/trackable';
 import RTTrPHeader from './rttrp-header';
@@ -8,4 +10,6 @@ export const Encoders = {
   RTTrPM,
   Trackable,
   CentroidPosition,
+  OrientationQuaternion,
+  OrientationEuler,
 };
